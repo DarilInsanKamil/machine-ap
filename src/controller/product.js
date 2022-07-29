b272f54cf51ea6810287cbb55a5448ccdde1272d
Darill <darilinsankamil2132@gmail.com>
2022-07-29 22:10:53 +0700

    add routing for crud

--- src/controller/product.js
+++ src/controller/product.js
@@ -1,7 +1,9 @@
-// [POST] ==> v1/product/post
 const { validationResult } = require('express-validator')
 const ProductPost = require('../model/product')
+const path = require('path')
+const fs = require('fs')
 
+// [POST] ==> v1/product/post
 exports.createProduct = (req, res, next) => {
     const errors = validationResult(req)
 
@@ -12,7 +14,7 @@ exports.createProduct = (req, res, next) => {
         throw err
     }
 
-    if(!req.file) {
+    if (!req.file) {
         const err = new Error('Image harus diupload')
         err.errorStatus = 422
         err.data = errors.array()
@@ -42,22 +44,118 @@ exports.createProduct = (req, res, next) => {
         })
 }
 
-
 // [GET] ==> v1/product/products
 exports.getAllProducts = (req, res, next) => {
-    const result = {
-        message: 'Create Data Sucsses',
-        data: {
-            id: +new Date(),
-            title: 'molding machine',
-            deskripsi: 'molding machine euro 4',
-            created_at: new Date().toLocaleDateString(),
-            author: {
-                uid: 1,
-                name: "komangg"
+    ProductPost.find()
+        .then(result => {
+            res.status(200).json({
+                message: 'Succsess get all data',
+                data: result
+            })
+        })
+        .catch(err => {
+            next(err)
+        })
+}
+
+// [GETbyID] ==> v1/product/products/:postId
+exports.getProductById = (req, res, next) => {
+    const postId = req.params.postId
+    ProductPost.findById(postId)
+        .then(result => {
+            if (!result) {
+                const error = new Error('Data tidak ditemukan')
+                error.errorStatus = 404
+                throw error
             }
-        }
+            res.status(200).json({
+                message: 'Data berhasil dipanggil',
+                data: result
+            })
+        })
+        .catch(err => {
+            next(err)
+        })
+}
+
+// [PUT/UPDATE] ==> v1/product/products/:postId
+exports.updateProductById = (req, res, next) => {
+    const errors = validationResult(req)
+
+    if (!errors.isEmpty()) {
+        const err = new Error('Invalid value')
+        err.errorStatus = 400
+        err.data = errors.array()
+        throw err
     }
-    res.status(200).json(result)
-    next()
+
+    if (!req.file) {
+        const err = new Error('Image harus diupload')
+        err.errorStatus = 422
+        err.data = errors.array()
+        throw err
+    }
+
+    const title = req.body.title
+    const image = req.file.path
+    const spesifikasi = req.file.path
+    const deskripsi = req.body.deskripsi
+    const postId = req.params.postId
+
+    ProductPost.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Data tidak ditemukan')
+                error.errorStatus = 404
+                throw error
+            }
+            post.title = title
+            post.deskripsi = deskripsi
+            post.image = image
+            post.spesifikasi = spesifikasi
+
+            return post.save()
+        })
+        .then(result => {
+            res.status(200).json({
+                message: 'Update Sukses',
+                data: result
+            })
+        })
+        .catch(err => {
+            next(err)
+        })
+}
+
+
+const removeImage = (filePath) => {
+    filePath = path.join(__dirname, '../..', filePath)
+    fs.unlink(filePath, err => console.log(err))
+}
+
+// [DELETE] ==> v1/product/products/:postId
+exports.deleteProductById = (req, res, next) => {
+    const postId = req.params.postId
+
+    ProductPost.findById(postId)
+        .then(post => {
+            if (!post) {
+                const err = new Error('Invalid value')
+                err.errorStatus = 400
+                err.data = errors.array()
+                throw err
+            }
+            removeImage(post.image)
+            // removeImage(post.spesifikasi)
+            return ProductPost.findByIdAndRemove(postId)
+        })
+        .then(result => {
+            res.status(200).json({
+                message: 'Berhasil dihapus',
+                data: result
+            })
+        })
+        .catch(err => {
+            next(err)
+        })
 }
--- src/routes/products.js
+++ src/routes/products.js
@@ -3,7 +3,7 @@ const { body } = require('express-validator')
 const router = express.Router()
 const productController = require('../controller/product')
 
-//Create Data => POST : localhost:4000/v1/product/post 
+//Create Data => [POST] : localhost:4000/v1/product/post 
 router.post('/post',
   [
     body('title').isLength({ min: 3 }).withMessage("input tidak sesuai"),
@@ -11,8 +11,21 @@ router.post('/post',
   ],
   productController.createProduct)
 
-//Get all data => GET : localhost:4000/v1/product/products 
+//Get all data => [GET] : localhost:4000/v1/product/products 
 router.get('/products', productController.getAllProducts)
 
+//Get data by ID => [GET] : localhost:4000/v1/product/products/:postId 
+router.get('/products/:postId', productController.getProductById)
+
+//Update data by ID => [PUT] : localhost:4000/v1/product/products/:postId 
+router.put('/products/:postId',
+  [
+    body('title').isLength({ min: 3 }).withMessage("input tidak sesuai"),
+    body('deskripsi').isLength({ min: 5 }).withMessage("input tidak sesuai")
+  ],
+  productController.updateProductById)
+
+//Delete data by ID => [DELETE] : localhost:4000/v1/product/products/:postId 
+router.delete('/products/:postId', productController.deleteProductById)
 
 module.exports = router
